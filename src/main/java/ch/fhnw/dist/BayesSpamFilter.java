843eac9b2b49b5bdebe96371604ca4c348bf46dc
Markus Winter <markus.winter@students.fhnw.ch>
2019-10-08 17:48:32 +0200

    filereader 2.0

--- src/main/java/ch/fhnw/dist/BayesSpamFilter.java
+++ src/main/java/ch/fhnw/dist/BayesSpamFilter.java
@@ -4,8 +4,10 @@ import java.io.File;
 
 public class BayesSpamFilter {
     public static void main(String[] args) {
-        File folder = new File("C:\\Users\\braini\\Dropbox\\FHNW\\Semester5\\dist\\#FHNW data copy\\Übungen\\Programmieraufgabe1\\ham-anlern");
+        File folderHam = new File("C:\\Users\\braini\\Dropbox\\FHNW\\Semester5\\dist\\#FHNW data copy\\Übungen\\Programmieraufgabe1\\ham-anlern");
+        //File folderSpam = new File("C:\\Users\\braini\\Dropbox\\FHNW\\Semester5\\dist\\#FHNW data copy\\Übungen\\Programmieraufgabe1\\spam-anlern");
         ReadFiles listFiles = new ReadFiles();
-        listFiles.listAllFiles(folder);
+        listFiles.listAllFiles(folderHam);
+        //listFiles.listAllFiles(folderSpam);
     }
 }
--- src/main/java/ch/fhnw/dist/ReadFiles.java
+++ src/main/java/ch/fhnw/dist/ReadFiles.java
@@ -4,18 +4,18 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ReadFiles {
     HashMap<String, WordModel> wordModels = new HashMap<>();
-    boolean isSpam = true;
+    boolean isSpam;
 
     public ReadFiles() {
 
     }
 
     public void listAllFiles(File folder){
-        System.out.println("In listAllfiles(File) method");
         File[] fileNames = folder.listFiles();
         for(File file : fileNames){
             // if directory call the same method again
@@ -30,24 +30,36 @@ public class ReadFiles {
 
             }
         }
+
+        wordModels.forEach((s, wordModel) -> {
+            double wordModelsSize = (double)wordModels.size()/100;
+            wordModel.setHamProbability(wordModel.getHamAmount()/wordModelsSize);
+            wordModel.setSpamProbability(wordModel.getSpamAmount()/wordModelsSize);
+        });
+
+        wordModels.forEach((s, wordModel) -> {
+            if(wordModel.getHamProbability() > 1) System.out.println(s + " ::::: " + wordModel.getHamProbability());
+        });
     }
 
     public void readContent(File file) throws IOException{
-        System.out.println("read file " + file.getCanonicalPath() );
+        //System.out.println("read file " + file.getCanonicalPath() );
         try(BufferedReader br  = new BufferedReader(new FileReader(file))){
             String strLine;
+            Set<String> wordList = new HashSet(); //auto distinct
+
+            isSpam = file.getName().contains("spam");
+
             // Read lines from the file, returns null when end of stream is reached
             while((strLine = br.readLine()) != null){
-                String[] currencies = strLine.split(" ");
-                for (String word : currencies){
-                    word = word.toUpperCase().trim();
-                    if(!word.isEmpty()) {
-                        //System.out.println(word);
-                        if(!wordModels.containsKey(word)) wordModels.put(word, new WordModel(isSpam));
-                        else wordModels.get(word).incAmount(isSpam);
-                    }
-                }
+                String[] wordSplitted = strLine.split(" ");
+                wordList.addAll(Arrays.stream(wordSplitted).map(s -> s.toUpperCase().trim()).filter(s -> !s.isEmpty()).collect(Collectors.toList()));
+            }
 
+            for (String word : wordList){
+                //System.out.println(word);
+                if(!wordModels.containsKey(word)) wordModels.put(word, new WordModel(isSpam));
+                else wordModels.get(word).incAmount(isSpam);
             }
         }
     }
