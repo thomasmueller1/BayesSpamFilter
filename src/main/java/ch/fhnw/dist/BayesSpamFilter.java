e75717d8d344a9bad49392ae00c0a7fe466ef69f
Markus Winter <markus.winter@students.fhnw.ch>
2019-10-08 21:43:12 +0200

    wordcount

--- src/main/java/ch/fhnw/dist/BayesSpamFilter.java
+++ src/main/java/ch/fhnw/dist/BayesSpamFilter.java
@@ -6,41 +6,85 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class BayesSpamFilter {
+    final static public String DEFAULT_TEMP_DIR = "C:/Temp"; // without final slash
+    final static public ClassLoader INTERNAL_FILE_PATH = BayesSpamFilter.class.getClassLoader();
+
     public static void main(String[] args) {
-        File folderHam = new File(System.getProperty("user.dir")+"\\src\\main\\resources\\ham-anlern");
-        File folderSpam = new File(System.getProperty("user.dir")+"\\src\\main\\resources\\spam-anlern");
-        ReadFiles listFiles = new ReadFiles();
-        listFiles.listAllFiles(folderHam, folderSpam);
-
-
         FileHelper fh = new FileHelper();
-        String zipFile = BayesSpamFilter.class.getClassLoader().getResource("ham-anlern.zip").getPath();
-        String zipFileTest = BayesSpamFilter.class.getClassLoader().getResource("ham-test.zip").getPath();
+        String zipHAM = INTERNAL_FILE_PATH.getResource("ham-anlern.zip").getPath();
+        String zipSPAM = INTERNAL_FILE_PATH.getResource("spam-anlern.zip").getPath();
+        String zipFileTest = INTERNAL_FILE_PATH.getResource("ham-test.zip").getPath();
 
-        String tempDir = "C:/Temp/unzipFolder";
-        ArrayList<String> results = new ArrayList<>();
-        HashMap<String, WordModel> list = new HashMap<>();
-        File f = new File(tempDir);
+
+        String tempDirHAM = DEFAULT_TEMP_DIR+"/ham-anlern";
+        String tempDirSPAM = DEFAULT_TEMP_DIR+"/spam-anlern";
+        ArrayList<String> resultsHAM = new ArrayList<>();
+        ArrayList<String> resultsSPAM = new ArrayList<>();
+
+        File fileHAM = new File(tempDirHAM);
+        File fileSPAM = new File(tempDirSPAM);
+
+        int countHAMmails = fileHAM.listFiles().length;
+        int countSPAMmails = fileSPAM.listFiles().length;
+
         try {
-            fh.unzip(zipFile, tempDir);
-            for (File file : fh.getAllFilesFromDirectory(f)) {
-                results.addAll(fh.readFileContentToList(file, false));
+            fh.unzip(zipHAM, tempDirHAM);
+            fh.unzip(zipSPAM, tempDirSPAM);
+            for (File file : fh.getAllFilesFromDirectory(fileHAM)) {
+                resultsHAM.addAll(fh.readFileContentToList(file, true));
+            }
+            for (File file : fh.getAllFilesFromDirectory(fileSPAM)) {
+                resultsSPAM.addAll(fh.readFileContentToList(file, true));
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
-        try{
-            fh.unzip(zipFileTest, "C:/Temp/test");
+        //System.out.println(zipSPAM);
+
+        HashMap<String, WordModel> resultsHAMNumMails = fh.createHashMapFromWords(resultsHAM, false);
+        HashMap<String, WordModel> resultsSPAMNumMails = fh.createHashMapFromWords(resultsSPAM, true);
+
+        HashMap<String, WordModel> resultsNumMails = resultsHAMNumMails;
+        resultsSPAMNumMails.forEach((s, wordModel) -> {
+            if(resultsNumMails.containsKey(s)) resultsNumMails.get(s).incSpamAmount();
+            else resultsNumMails.put(s, wordModel);
+        });
+
+        /*resultsHAMNumMails.forEach((s, wordModel) -> System.out.println(s + " : " + wordModel.getHamAmount()));
+        System.out.println("-------------");
+        resultsSPAMNumMails.forEach((s, wordModel) -> System.out.println(s + " : " + wordModel.getHamAmount()));*/
+
+        resultsNumMails.forEach((s, wordModel) -> {
+            wordModel.setHamProbability((double)wordModel.getHamAmount()/countHAMmails*100);
+            wordModel.setSpamProbability((double)wordModel.getSpamAmount()/countSPAMmails*100);
+        });
+
+        /*resultsHAMNumMails.forEach((s, wordModel) -> {
+            System.out.println(s + ": " + wordModel.getHamAmount() + " | " + wordModel.getSpamAmount());
+        });*/
+
+        System.out.println("----------");
+
+        /*resultsSPAMNumMails.forEach((s, wordModel) -> {
+            System.out.println(s + ": " + wordModel.getHamAmount() + " | " + wordModel.getSpamAmount());
+        });*/
+
+        resultsNumMails.forEach((s, wordModel) -> {
+            System.out.println(s + ": " + wordModel.getHamAmount() + "("+wordModel.getHamProbability()+") | " + wordModel.getSpamAmount()+" ("+wordModel.getSpamProbability());
+        });
+
+        /*try{
+            fh.unzip(zipFileTest, DEFAULT_TEMP_DIR+"/test");
             list = fh.createHashMapFromWords(results, false);
-            ArrayList<File> mails =  fh.getAllFilesFromDirectory(new File("C:/Temp/test"));
+            ArrayList<File> mails =  fh.getAllFilesFromDirectory(new File(DEFAULT_TEMP_DIR+"/test"));
             ArrayList<String> firstMailWords = fh.readFileContentToList(mails.get(0),false);
             for (String mailWord : firstMailWords){
                 System.out.println(mailWord + " "+ calcPajassAlgo(firstMailWords,list));
             }
         }catch (Exception e){
 
-        }
+        }*/
 
 
     }
@@ -57,5 +101,10 @@ public class BayesSpamFilter {
             }
         }
         return spamProduct / (spamProduct + hamProduct);
+    }
+
+
+    public static void setProbabilities(HashMap<String, WordModel> words, int numMails) {
+
     }
 }
--- src/main/java/ch/fhnw/dist/WordModel.java
+++ src/main/java/ch/fhnw/dist/WordModel.java
@@ -33,7 +33,6 @@ public class WordModel
         SpamAmount++;
     }
 
-
     public double getSpamProbability() {
         return SpamProbability;
     }
