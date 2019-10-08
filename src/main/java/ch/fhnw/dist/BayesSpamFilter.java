bbbcd196680c5766ba22294dea993b8e90550232
Markus Winter <markus.winter@students.fhnw.ch>
2019-10-08 16:41:27 +0200

    filereader

--- src/main/java/ch/fhnw/dist/BayesSpamFilter.java
+++ src/main/java/ch/fhnw/dist/BayesSpamFilter.java
@@ -1,7 +1,13 @@
 package ch.fhnw.dist;
 
+import java.io.File;
+
 public class BayesSpamFilter {
     public static void main(String[] args) {
         System.out.println("test");
+
+        File folder = new File("C:\\Users\\braini\\Dropbox\\FHNW\\Semester5\\dist\\#FHNW data copy\\Übungen\\Programmieraufgabe1\\ham-anlern");
+        ReadFiles listFiles = new ReadFiles();
+        listFiles.listAllFiles(folder);
     }
 }
