7b32db699ba4bfb46dbc07790b32c925caedad48
Thierry Hundt <thierry.hundt@students.fhnw.ch>
2019-10-08 19:28:24 +0200

    thierry try algo

--- src/main/java/ch/fhnw/dist/BayesSpamFilter.java
+++ src/main/java/ch/fhnw/dist/BayesSpamFilter.java
@@ -1,8 +1,9 @@
 package ch.fhnw.dist;
 
+
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class BayesSpamFilter {
     public static void main(String[] args) {
@@ -13,21 +14,49 @@ public class BayesSpamFilter {
         //listFiles.listAllFiles(folderSpam);
 
 
-
         FileHelper fh = new FileHelper();
         String zipFile = BayesSpamFilter.class.getClassLoader().getResource("ham-anlern.zip").getPath();
+        String zipFileTest = BayesSpamFilter.class.getClassLoader().getResource("ham-test.zip").getPath();
+
         String tempDir = "C:/Temp/unzipFolder";
         ArrayList<String> results = new ArrayList<>();
+        HashMap<String, WordModel> list = new HashMap<>();
         File f = new File(tempDir);
         try {
             fh.unzip(zipFile, tempDir);
             for (File file : fh.getAllFilesFromDirectory(f)) {
-                results.addAll(fh.readFileContentToList(file,false));
+                results.addAll(fh.readFileContentToList(file, false));
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
-        results.forEach(System.out::println);
+        try{
+            fh.unzip(zipFileTest, "C:/Temp/test");
+            list = fh.createHashMapFromWords(results, false);
+            ArrayList<File> mails =  fh.getAllFilesFromDirectory(new File("C:/Temp/test"));
+            ArrayList<String> firstMailWords = fh.readFileContentToList(mails.get(0),false);
+            for (String mailWord : firstMailWords){
+                System.out.println(mailWord + " "+ calcPajassAlgo(firstMailWords,list));
+            }
+        }catch (Exception e){
+
+        }
+
+
+    }
+
+
+    public static double calcPajassAlgo(ArrayList<String> wordsFormMail, HashMap<String, WordModel> validationList) {
+        double spamProduct = 0;
+        double hamProduct = 0;
+
+        for (String word : wordsFormMail) {
+            if (validationList.containsKey(word)) {
+                spamProduct *= validationList.get(word).getSpamProbability();
+                hamProduct *= validationList.get(word).getHamProbability();
+            }
+        }
+        return spamProduct / (spamProduct + hamProduct);
     }
 }
