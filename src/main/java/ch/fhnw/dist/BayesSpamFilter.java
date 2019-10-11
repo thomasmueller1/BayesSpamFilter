e1041cc11667e86fa0aec0ac9499ab308ceb5ad1
Markus Winter <markus.winter@students.fhnw.ch>
2019-10-11 11:46:26 +0200

    added banner

--- src/main/java/ch/fhnw/dist/BayesSpamFilter.java
+++ src/main/java/ch/fhnw/dist/BayesSpamFilter.java
@@ -14,6 +14,9 @@ public class BayesSpamFilter {
         HashMap<String, Integer> resultsHam  = filter.determineSpamProbability(wordList, CONFIG.HAM_MAILS_TO_TEST_ZIP); // Spamwahrscheinlichkeit in Ham Testmails
         HashMap<String, Integer> resultsSpam = filter.determineSpamProbability(wordList, CONFIG.SPAM_MAILS_TO_TEST_ZIP); // Spamwahrscheinlichkeit in Spam Testmails
 
+        /* Banner ausgeben */
+        showBanner();
+
         /* Allgemeine Infos ausgeben */
         System.out.println("Schwellwert: " + CONFIG.SPAM_THRESHOLD);
         System.out.println("Alpha Wert:  " + CONFIG.PROBABILITY_ON_ZEROMAILS);
@@ -36,6 +39,17 @@ public class BayesSpamFilter {
 
         System.out.printf("Ham:  %s%% (%d)%n", Math.round(ham/total*100*100.0)/100.0, ham);
         System.out.printf("Spam: %s%% (%d)%n", Math.round(spam/total*100*100.0)/100.0, spam);
+    }
+
+    private static  void showBanner() {
+        System.out.println(" ____      __     ________  _____    _____                                  ______ _ _ _            ");
+        System.out.println("|  _ \\   /\\\\ \\   / /  ____|/ ____|  / ____|                                |  ____(_) | |           ");
+        System.out.println("| |_) | /  \\\\ \\_/ /| |__  | (___   | (___  _ __   __ _ _ __ ___    ______  | |__   _| | |_ ___ _ __ ");
+        System.out.println("|  _ < / /\\ \\\\   / |  __|  \\___ \\   \\___ \\| '_ \\ / _` | '_ ` _ \\  |______| |  __| | | | __/ _ \\ '__|");
+        System.out.println("| |_) / ____ \\| |  | |____ ____) |  ____) | |_) | (_| | | | | | |          | |    | | | ||  __/ |   ");
+        System.out.println("|____/_/    \\_\\_|  |______|_____/  |_____/| .__/ \\__,_|_| |_| |_|          |_|    |_|_|\\__\\___|_|   ");
+        System.out.println("                                          | |                                                       ");
+        System.out.println("                                          |_|         by Thierry Hundt, Thomas Müller, Markus Winter\n");
     }
 
 
